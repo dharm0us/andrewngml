7c03cff27687894d84681a6b7904953475b5e36c
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2017-02-28 14:45:10 +0530

    first part of assignment

--- machine-learning-ex8/ex8/estimateGaussian.m
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -13,7 +13,8 @@ function [mu sigma2] = estimateGaussian(X)
 % You should return these values correctly
 mu = zeros(n, 1);
 sigma2 = zeros(n, 1);
-
+mu = mean(X,1)
+sigma2 = var(X-mu,1)
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the mean of the data and the variances
 %               In particular, mu(i) should contain the mean of
