8e85a824d1fe5ef5971ba6f2b1470a28e2746c85
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2017-02-28 16:54:58 +0530

    select threshold multi variate gaussian anamoly detection

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -12,7 +12,6 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
@@ -23,23 +22,30 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    predictions = (pval < epsilon);
+    tp = sum((predictions == 1) & yval == 1) ;  
+    fp = sum((predictions == 1) & yval == 0);
+    fn = sum((predictions == 0) & yval == 1);
 
-
-
-
-
-
-
-
-
-
+     prec = 0;
+     rec  = 0 ;   
+    if tp + fp > 0 &&  tp + fn > 0
+      prec = tp/(tp + fp);
+      rec = tp/(tp + fn);
+    end
+  
+    if prec + rec > 0
+      F1 = 2 * prec * rec/(prec + rec);   
+    end
 
 
     % =============================================================
 
     if F1 > bestF1
-       bestF1 = F1;
-       bestEpsilon = epsilon;
+      prec
+      rec
+       bestF1 = F1
+       bestEpsilon = epsilon
     end
 end
 
