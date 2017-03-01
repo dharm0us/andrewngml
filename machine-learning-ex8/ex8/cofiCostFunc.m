6dc54b989cfe5a98b6172f4f823a84fb9cc8088f
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2017-03-01 16:00:56 +0530

    regularize cost and gradients

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -16,12 +16,16 @@ Theta = reshape(params(num_movies*num_features+1:end), ...
 J = 0;
 X_grad = zeros(size(X));
 Theta_grad = zeros(size(Theta));
-tmp = (X*Theta' - Y).*R
-tmp = tmp .* tmp
+tmp = (X*Theta' - Y).*R;
+tmp = tmp .* tmp;
 J = sum(sum(tmp))/2;
 
-X_grad = ((X*Theta' - Y).*R)*Theta
-Theta_grad = ((X*Theta' - Y).*R)'*X
+%regularization
+J = J + (lambda/2)*sum(sum(X.*X));
+J = J + (lambda/2)*sum(sum(Theta.*Theta));
+
+X_grad = ((X*Theta' - Y).*R)*Theta + lambda * X;
+Theta_grad = ((X*Theta' - Y).*R)'*X + lambda * Theta;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost function and gradient for collaborative
