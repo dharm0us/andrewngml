e775e5eab1dc8d81943bce03f91cef6857406891
dharam@epicenterlabs.com <dharam@epicenterlabs.com>
2017-03-26 01:58:18 +0530

    iterate and choose best C,sigma

--- machine-learning-ex6/machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/machine-learning-ex6/ex6/dataset3Params.m
@@ -10,7 +10,29 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+        predictions = svmPredict(model, Xval);
+ error = mean(double(predictions ~= yval));
 
+
+C_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+sigma_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
+for i = 1:length(C_vec)
+          curr_C = C_vec(i);
+  for j = 1:length(sigma_vec)
+        curr_sigma = sigma_vec(j);
+        model= svmTrain(X, y, curr_C, @(x1, x2) gaussianKernel(x1, x2, curr_sigma)); 
+predictions = svmPredict(model, Xval);
+ curr_error = mean(double(predictions ~= yval));
+if (curr_error < error)
+  error = curr_error;
+  sigma = curr_sigma;
+  C = curr_C;
+  
+  endif
+        end
+      end
+      
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -22,8 +44,7 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
+ 
 
 
 
